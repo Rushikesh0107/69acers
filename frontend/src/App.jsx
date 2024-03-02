9e8e04dd48c5ac995f7abfcd37c2a078694914bc
Rushikesh0107 <rushikeshdadas03@gmail.com>
2024-03-02 15:32:14 +0530

    Add SignUp page and form components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Home from  "./pages/Home"
 import Login_Page from './pages/Login_Page'
+import SignUp_Page from './pages/SignUp_page'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
           <Route path='/' element={<Home />} />
           <Route path="/login" element={<Login_Page />} />
+          <Route path='/signup' element={<SignUp_Page />} />
       </Routes>
     </>
   )
---
+++ frontend/src/pages/SignUp_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from '../components/core/Auth/SignUp_Form'
+
+const SignUp_page = () => {
+  return (
+    <div>
+        <SignUpForm />
+    </div>
+  )
+}
+
+export default SignUp_page
